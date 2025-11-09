8e329a475fbdab66e07b4783b9b3d85032dcb8b5
Eric Chea <dodosmarts@protonmail.com>
2025-11-09 07:04:32 -0500

    Add regex masker.

---
+++ src/veildata/maskers.py
@@ -0,0 +1,13 @@
+import re
+
+from .core import Module
+
+
+class RegexMasker(Module):
+    def __init__(self, pattern, mask_token="[REDACTED]"):
+        super().__init__()
+        self.pattern = re.compile(pattern)
+        self.mask_token = mask_token
+
+    def forward(self, text):
+        return self.pattern.sub(self.mask_token, text)
