4c2978d0c5b1907cfdd72264a1a28a4f7243f643
Eric Chea <dodosmarts@protonmail.com>
2025-11-09 18:46:51 -0500

    Fix imports

--- src/veildata/__init__.py
+++ src/veildata/__init__.py
@@ -1,7 +1,7 @@
-from src.veildata.core import Module
-from src.veildata.maskers import BERTNERMasker, RegexMasker, SpacyNERMasker
-from src.veildata.revealers import TokenStore
-from src.veildata.transforms import Compose
+from veildata.core import Module
+from veildata.maskers import BERTNERMasker, RegexMasker, SpacyNERMasker
+from veildata.revealers import TokenStore
+from veildata.transforms import Compose
 
 __all__ = [
     "Module",
